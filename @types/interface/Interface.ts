4f0852af50a6a495faa92399a7cb4a01225c79ec
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-07-28 15:15:02 +0530

    Update

--- @types/interface/Interface.ts
+++
@@ -1,7 +0,0 @@
-// Creating an interface prop type
-export interface RNChildProp {
-  children?: React.ReactNode;
- }
- 
- // children is a special prop in React which is used to pass children
- // to a parent component between components.
--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Dashboard() {
-	return (
-		<main>
-			<div>
-				<h1>Dashboard</h1>
-			</div>
-		</main>
-	);
-}
